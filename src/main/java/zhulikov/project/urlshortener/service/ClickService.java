c90a7e4da7e1b31957177ab348e72dd010094a2e
Stazher0 <zhulikov152@gmail.com>
2026-08-31 20:29:45 +0300

    feat: add Click entity and track redirect clicks in PostgreSQL

--- src/main/java/zhulikov/project/urlshortener/service/ClickService.java
+++ src/main/java/zhulikov/project/urlshortener/service/ClickService.java
@@ -1,13 +1,18 @@
 package zhulikov.project.urlshortener.service;
 
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import zhulikov.project.urlshortener.model.Click;
 import zhulikov.project.urlshortener.model.Url;
 import zhulikov.project.urlshortener.repository.ClickRepo;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 @Service
 @Transactional
@@ -16,12 +21,29 @@ public class ClickService {
 
     private final ClickRepo clickRepo;
 
+    private final BlockingQueue <Click> clickQueue =  new LinkedBlockingQueue<>();
+
     public void createClickData(Url urlModel) {
         Click clickModel = new Click();
 
         clickModel.setUrl(urlModel);
         clickModel.setClickedAt(LocalDateTime.now());
 
-        clickRepo.save(clickModel);
+        addClickToQueue(clickModel);
+    }
+
+    public void addClickToQueue(Click click) {
+        clickQueue.offer(click);
+    }
+
+    @Scheduled(fixedDelay = 5000)
+    public void sendClickData(){
+        List<Click> clicks = new ArrayList<>();
+        clickQueue.drainTo(clicks);
+
+        if (!clicks.isEmpty()){
+            clickRepo.saveAll(clicks);
+            System.out.println("Saved " + clicks.size() + " clicks");
+        }
     }
 }
